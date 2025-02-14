62e7695f8129490e2d380b561c5907d695d43d96
Anonymous <your@email.com>
2025-02-14 10:10:11 +0800

    Your commit message

---
+++ src/main/java/edu/nju/seg/isd/sisd/lab/Lab.java
@@ -0,0 +1,38 @@
+package edu.nju.seg.isd.sisd.lab;
+
+import edu.nju.seg.isd.sisd.block.BlockChecker;
+import edu.nju.seg.isd.sisd.exception.ExceptionHandler;
+import org.sosy_lab.java_smt.api.SolverException;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Lab {
+
+    public static void main(String[] args) {
+        String b="hello";
+        Arrays.asList(b.split(""));
+        if (args.length ==1) {
+            File files = Cases.genFile(args[0]);
+            new Lab().run(files, true, true);
+        }
+        else if (args.length > 1) {
+            File files = Cases.genFile(args[0]);
+            new Lab().run(files, Boolean.valueOf(args[1]), Boolean.valueOf(args[2]));
+        }
+        else System.out.println("please provide params about filename or option of tactic.");
+    }
+
+    public void run(File file, Boolean subducted, Boolean pruned) {
+        if (!subducted)System.out.println("Tactic 1 is closed.");
+        if (!pruned)System.out.println("Tactic 2 is closed.");
+        var checker = new BlockChecker(file, subducted, pruned);
+        try {
+            checker.check();
+        } catch (InterruptedException | SolverException e) {
+            ExceptionHandler.handle(e);
+        }
+    }
+
+}
